137b51bd39f740ab872e27a5055efe3418aef6f6
Pedro Paulo <pedropaulodfr@outlook.com>
2022-12-16 12:22:12 -0300

    Card-Icons | Valores

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,15 +1,19 @@
 import React from 'react'
 import ResumeItem from '../ResumeItem'
-
 import * as C from './styles'
+import { 
+  FaRegArrowAltCircleUp,
+  FaRegArrowAltCircleDown,
+  FaDollarSign
+} from 'react-icons/fa'
 
 const Resume = () => {
   return (
     <>
         <C.Container>
-          <ResumeItem></ResumeItem>
-          <ResumeItem></ResumeItem>
-          <ResumeItem></ResumeItem>
+          <ResumeItem title="Entradas" Icon={FaRegArrowAltCircleUp} value='1000'></ResumeItem>
+          <ResumeItem title="Saídas" Icon={FaRegArrowAltCircleDown} value='400'></ResumeItem>
+          <ResumeItem title="Total" Icon={FaDollarSign} value='600'></ResumeItem>
         </C.Container>
     </>
 
